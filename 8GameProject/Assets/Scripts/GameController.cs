9b49a3090adf0488e07ebd805802a6a4403c8e05
Edd Brown <edd_brown@bloomfield.edu>
2015-11-29 07:01:12 -0500

    basic
    basic structure no goals

---
+++ 8GameProject/Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject spheresHaz;
+	public Canvas titleUI;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+			
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void ToMainLevel()
+	{
+
+		Application.LoadLevel (1);
+		titleUI.enabled = false;
+	}
+	
+
+}
