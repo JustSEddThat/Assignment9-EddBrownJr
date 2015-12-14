830cb755d18a447f20ad54d29c7c6cd01283edd0
Edd Brown <edd_brown@bloomfield.edu>
2015-12-14 13:28:42 -0500

    begining
    gamecontroller needs work othersss

---
+++ 8GameProject/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript: MonoBehaviour 
+{
+	public void nextLevel()
+	{
+		Application.LoadLevel (1);
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
